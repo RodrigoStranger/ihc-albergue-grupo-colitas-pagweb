4e1eded00ed56ad8b84bf0d52317ea00495e3e3d
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-05-04 10:51:54 -0500

    update

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -11,29 +11,27 @@ function Footer() {
           <h3>Grupo Colitas</h3>
           <p>Dando amor y esperanza a los animales necesitados desde 2024</p>
         </div>
-        <div className="footer-section">
+        <div className="footer-section social-section">
           <h3>Síguenos</h3>
           <div className="social-links">
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.facebook.com/profile.php?id=100088175020547&rdid=vqISWL2JHRi89qNO&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F1AKkzZcoLX%2F" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-facebook"></i>
             </a>
-            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-instagram"></i>
-            </a>
-            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-youtube"></i>
+            <a href="https://www.tiktok.com/@grupocolitasarequipa?_t=ZM-8vaS8VtvcAv&_r=1" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-tiktok"></i>
             </a>
           </div>
         </div>
       </div>
       <div className="footer-bottom">
-        <p>&copy; {currentYear} Grupo Colitas. Todos los derechos reservados.</p>
+        <p>
+          &copy; {currentYear} Grupo Colitas. Todos los derechos reservados.
+          <br />
+          Desarrollado por estudiantes de <a href="https://www.lasalle.edu.pe/arequipa" target="_blank" rel="noopener noreferrer">Universidad La Salle de Arequipa</a>
+        </p>
       </div>
     </footer>
   );
 }
 
-export default Footer; 
+export default Footer;
