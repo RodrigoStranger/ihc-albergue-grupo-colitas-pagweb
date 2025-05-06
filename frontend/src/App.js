7e333b502a04031bbb3db968736cb62095b104d2
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-05-06 14:32:41 -0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,25 +7,30 @@ import Voluntariado from './pages/Voluntariado';
 import NotFound from './pages/NotFound';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <ScrollToTop />
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" replace />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/adoptar" element={<Adoptar />} />
-          <Route path="/donar" element={<Donar />} />
-          <Route path="/voluntariado" element={<Voluntariado />} />
-          {/* Ruta comodín para páginas no encontradas */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" replace />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/adoptar" element={<Adoptar />} />
+            <Route path="/donar" element={<Donar />} />
+            <Route path="/voluntariado" element={<Voluntariado />} />
+            {/* Ruta comodín para páginas no encontradas */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
         <Footer />
       </div>
     </Router>
   );
 }
+
 export default App;
