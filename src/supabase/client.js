16afd276b9374fd1258cce54c7b4919d1579c3a0
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-05-15 13:47:55 -0500

    update

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Cargar variables de entorno directamente desde .env
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://edvhcblegytbfbneujae.supabase.co';
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkdmhjYmxlZ3l0YmZibmV1amFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczMTM2NjYsImV4cCI6MjA2Mjg4OTY2Nn0.AoZGKDoTzBxl_kzBAV-C_QLCJ9eUzOISKqgEWm2zdmw';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const client = createClient(supabaseUrl, supabaseAnonKey);
 
-export default supabase;
+export default client;
